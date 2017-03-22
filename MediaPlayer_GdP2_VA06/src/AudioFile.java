3381f44ff61d2af4d77213b3c6d71a38fb65a1f3
Phil Z <phil.zenzger@gmail.com>
2017-03-22 17:03:43 +0100

    1st start of VA06

---
+++ MediaPlayer_GdP2_VA06/src/AudioFile.java
@@ -0,0 +1,37 @@
+
+public class AudioFile {
+
+	/* -------------------------------------------------- */
+	// Globals
+	public String path;
+	public String pathname;
+	public String filename;
+	
+	// constructor
+	public AudioFile() {
+	
+	}
+	
+	
+	// modify Pathname
+	public void parsePathname(String s) {
+		
+	}
+	
+	/* ------------------------------------------------- */
+	// Getters
+	public String getPathname() {
+		return pathname;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
+	
+	
+	
+	
+}
+
+
+
